a037ce434eb98901bfa71aff3626a547de9370e8
NishantRanjan-nr <nishantranjan910@gmail.com>
2026-05-23 14:40:51 +0530

    chore(backend): use bcryptjs, add start script and node engines; remove backend lockfile

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwtToken from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
